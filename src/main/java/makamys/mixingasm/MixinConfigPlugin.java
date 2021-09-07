ce08e85d4f28920e3916e862f3abe52c4611f8bb
makamys <makamys@outlook.com>
2021-09-07 13:27:59 +0200

    Initial commit. Adds a hardcoded transformer exclusion.

---
+++ src/main/java/makamys/mixingasm/MixinConfigPlugin.java
@@ -0,0 +1,49 @@
+package makamys.mixingasm;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import org.spongepowered.asm.lib.tree.ClassNode;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+public class MixinConfigPlugin implements IMixinConfigPlugin {
+    
+    @Override
+    public void onLoad(String mixinPackage) {
+        MixinEnvironment.getCurrentEnvironment().addTransformerExclusion("net.tclproject.mysteriumlib.asm.common.CustomClassTransformer");
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+        
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return Arrays.asList();
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+        
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+        
+    }
+
+}
